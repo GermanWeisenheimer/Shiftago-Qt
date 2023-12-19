4637f1999d70d454076e71d7f79e76ae720e2a0a
Thomas Schaper <genius.magnus@tutanota.com>
2023-12-19 18:11:17 +0100

    Docstrings added.

--- shiftago/ui/app_events.py
+++ shiftago/ui/app_events.py
@@ -5,25 +5,28 @@ from .hmvc import AppEvent
 
 @dataclass(frozen=True)
 class MoveSelectedEvent(AppEvent):
+    """Emitted by view and thinking worker."""
     move: Move
 
 
 @dataclass(frozen=True)
 class AnimationFinishedEvent(AppEvent):
-    pass
+    """Emitted by view."""
 
 
 @dataclass(frozen=True)
 class ExitRequestedEvent(AppEvent):
-    pass
+    """Emitted by view."""
 
 
 @dataclass(frozen=True)
 class MarbleShiftedEvent(AppEvent):
+    """Emitted by model."""
     slot: Slot
     direction: Side
 
 
 @dataclass(frozen=True)
 class MarbleInsertedEvent(AppEvent):
+    """Emitted by model."""
     slot: Slot
