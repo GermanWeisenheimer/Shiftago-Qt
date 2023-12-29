3d2e46e36f45d6054853d7c8811fee6ec338f311
Thomas Schaper <genius.magnus@tutanota.com>
2023-12-29 11:03:45 +0100

    Log level now set via config file.

--- shiftago/__main__.py
+++ shiftago/__main__.py
@@ -1,15 +1,40 @@
 import sys
 import logging
+from configparser import ConfigParser
 from pathlib import Path
 from datetime import datetime
 from types import TracebackType
+from typing import NamedTuple
 from PySide2.QtCore import QThread
 from .ui.shiftago_qt_express import ShiftagoQtExpress
 
+_CONFIG_FILE = 'shiftago-qt.cfg'
 _logger = logging.getLogger(__name__)
 
 
-def _configure_logging(*, logs_dir: str = './logs', filename_prefix: str = 'shiftago_qt') -> str:
+class _ShiftagoConfig(NamedTuple):
+    log_level: int
+
+
+def _read_config() -> _ShiftagoConfig:
+    config_parser = ConfigParser()
+    log_level_to_use = logging.INFO
+    if config_parser.read(_CONFIG_FILE):
+        try:
+            parsed_log_level = logging.getLevelName(config_parser['logging']['level'])
+            if isinstance(parsed_log_level, int):
+                log_level_to_use = parsed_log_level
+            else:
+                _logger.error("Illegal logging level in configuration file: %s", parsed_log_level)
+        except KeyError:
+            _logger.warning("Logging level not present in configuration file.")
+    else:
+        _logger.warning("Configuration file '%s' not found.", _CONFIG_FILE)
+    return _ShiftagoConfig(log_level=log_level_to_use)
+
+
+def _configure_logging(*, logs_dir: str = './logs', filename_prefix: str = 'shiftago_qt',
+                       config: _ShiftagoConfig) -> str:
     '''Adds the name of the current QThread as field 'threadName'.'''
     def thread_name_filter(record: logging.LogRecord):
         qthread_name = QThread.currentThread().objectName()
@@ -21,7 +46,7 @@ def _configure_logging(*, logs_dir: str = './logs', filename_prefix: str = 'shif
     handlers = [logging.StreamHandler(), logging.FileHandler(filename, mode='w')]
     for handler in handlers:
         handler.addFilter(thread_name_filter)
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=config.log_level,
                         format='%(asctime)s [%(threadName)14s] %(name)s %(levelname)5s - %(message)s',
                         handlers=handlers)
     return filename
@@ -37,5 +62,6 @@ if __name__ == '__main__':
         sys.exit(1)
 
     sys.excepthook = handle_uncaught_exception
-    print(f"Writing log file {_configure_logging()}")
+    cfg = _read_config()
+    print(f"Writing log file {_configure_logging(config=cfg)}")
     sys.exit(ShiftagoQtExpress(sys.argv).exec_())
