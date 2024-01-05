f6de04a7c1af2659663da5b27484e407bd76e6f1
Thomas Schaper <genius.magnus@tutanota.com>
2024-01-05 07:43:37 +0100

    Batch script improved.

--- make_dist.bat
+++ make_dist.bat
@@ -1,18 +1,21 @@
 @echo off
 setlocal
 
+set scripts_dir=.venv\Scripts
+
 rem generate spec file
 set app_name=shiftago-qt
 set images_dir=shiftago/ui/images
-.venv\Scripts\pyi-makespec entry_point.py --name=%app_name% --add-data "%images_dir%/*.png:%images_dir%" --add-data "%images_dir%/*.jpg:%images_dir%"
+%scripts_dir%\pyi-makespec entry_point.py --name=%app_name%^
+    --add-data "%images_dir%/*.png:%images_dir%"^
+    --add-data "%images_dir%/*.jpg:%images_dir%"
 
 rem enhance spec file
 set spec_file=%app_name%.spec
-set config_file=shiftago-qt.cfg
 echo import shutil>>%spec_file%
-echo shutil.copyfile('%config_file%', f'{DISTPATH}/%app_name%/%config_file%')>>%spec_file%
+echo shutil.copy('shiftago-qt.cfg', f'{DISTPATH}/{specnm}')>>%spec_file%
 
 rem run installer with enhanced spec file
-.venv\Scripts\pyinstaller --clean --noconfirm %spec_file%
+%scripts_dir%\pyinstaller --clean --noconfirm %spec_file%
 
 endlocal
