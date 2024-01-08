1df4ed3189342d4ab9fbc0d6d1425305d7a08581
Thomas Schaper <genius.magnus@tutanota.com>
2024-01-08 22:23:46 +0100

    AlphaBetaPruning refactored.

--- shiftago/core/express_ai.py
+++ shiftago/core/express_ai.py
@@ -84,27 +84,81 @@ class AlphaBetaPruning(AIEngine[ShiftagoExpress]):
             self._minimizer = self.Minimizer()
 
         @property
-        def current_depth(self) -> int:
-            return self._current_depth
-
-        @property
         def current_strategy(self) -> _MiniMaxStrategy:
             return self._maximizer if self._current_depth % 2 == 1 else self._minimizer
 
-        @property
-        def num_occupied_slots(self) -> int:
-            return self._num_occupied_slots
-
-        def down(self):
+        def _down(self):
             self._current_depth += 1
             self._num_occupied_slots += 1
 
-        def up(self):
+        def _up(self):
             self._current_depth -= 1
             self._num_occupied_slots -= 1
 
-        def should_recurse(self) -> bool:
-            return self._num_occupied_slots >= 6 and self._current_depth < self._max_depth
+        def _should_recurse(self, node: _Node) -> bool:
+            return not node.is_leaf and node.depth < self._max_depth and self._num_occupied_slots >= 6
+
+        def apply(self, game_state: ShiftagoExpress, alpha: float, beta: float) -> Tuple[Move, _Node, int]:
+            assert game_state.current_player, "No current player!"
+            current_strategy = self.current_strategy
+            possible_moves = game_state.detect_all_possible_moves()
+            nodes = {move: self._eval_move(game_state, move) for move in possible_moves}
+            if self._num_occupied_slots <= 1:
+                random_move = random.choice(possible_moves)
+                return random_move, nodes[random_move], 1
+            possible_moves.sort(key=lambda m: nodes[m].rating, reverse=current_strategy.is_maximizing)
+            num_visited_nodes = 0
+            optimal_move = None  # type: Optional[Move]
+            for current_move in possible_moves:
+                current_node = nodes[current_move]
+                if self._should_recurse(current_node):
+                    self._down()
+                    _, child_node, child_num_visited = self.apply(current_node.new_game_state, alpha, beta)
+                    self._up()
+                    num_visited_nodes += child_num_visited
+                    current_node.depth = child_node.depth
+                    current_node.rating = child_node.rating
+                else:
+                    num_visited_nodes += 1
+                if current_strategy.is_maximizing:
+                    if optimal_move is None or current_node.rating > alpha:
+                        optimal_move = current_move
+                        alpha = current_node.rating
+                        if alpha >= beta or alpha == current_strategy.win_rating:
+                            break  # cut-off!
+                else:
+                    if optimal_move is None or current_node.rating < beta:
+                        optimal_move = current_move
+                        beta = current_node.rating
+                        if beta <= alpha or beta == current_strategy.win_rating:
+                            break  # cut-off!
+            assert optimal_move is not None
+            return optimal_move, nodes[optimal_move], num_visited_nodes
+
+        def _eval_move(self, game_state: ShiftagoExpress, move: Move) -> _Node:
+            is_leaf = False
+            rating = 0.0
+            cloned_game_state = game_state.clone()
+            game_end_condition = cloned_game_state.apply_move(move)
+            if game_end_condition is not None:
+                is_leaf = True
+                if game_end_condition.winner is not None:
+                    rating = self.current_strategy.win_rating
+            else:
+                player_results = cloned_game_state.analyze()
+                assert game_state.current_player, "No current player!"
+                current_player_result = player_results[game_state.current_player]
+                opponent_result = player_results[self._current_opponent(game_state)]
+                winning_line_length = game_state.winning_line_length
+                win_rating = self.current_strategy.win_rating
+                for i in range(winning_line_length, 1, -1):
+                    rating += (len(current_player_result[i]) - len(opponent_result[i])
+                               ) * _pow10(-(winning_line_length - i + 1)) * win_rating
+            return _Node(cloned_game_state, is_leaf, self._current_depth, rating)
+
+        def _current_opponent(self, game_state: ShiftagoExpress) -> Colour:
+            players = game_state.players
+            return players[1 if players.index(game_state.current_player) == 0 else 0]
 
     def __init__(self, skill_level=SkillLevel.ADVANCED) -> None:
         super().__init__(skill_level)
@@ -113,76 +167,8 @@ class AlphaBetaPruning(AIEngine[ShiftagoExpress]):
     def select_move(self, game_state: ShiftagoExpress) -> Move:
         assert len(game_state.players) == 2
         assert game_state.current_player, "No current player!"
-        move, node, num_visited_nodes = self._apply_mini_max(self._MiniMax(self._max_depth,
-                                                                           game_state.count_occupied_slots()),
-                                                             game_state, -math.inf, math.inf)
+        move, node, num_visited_nodes = self._MiniMax(
+            self._max_depth, game_state.count_occupied_slots()).apply(game_state, -math.inf, math.inf)
         _logger.debug("Selected move: %s (depth = %d, rating = %f, num_visited_nodes = %d)",
                       move, node.depth, node.rating, num_visited_nodes)
         return move
-
-    @staticmethod
-    def _apply_mini_max(mini_max: _MiniMax, game_state: ShiftagoExpress,
-                        alpha: float, beta: float) -> Tuple[Move, _Node, int]:
-        assert game_state.current_player, "No current player!"
-        current_strategy = mini_max.current_strategy
-        possible_moves = game_state.detect_all_possible_moves()
-        nodes = {move: AlphaBetaPruning._eval_move(mini_max, game_state, move) for move in possible_moves}
-        if mini_max.num_occupied_slots <= 1:
-            random_move = random.choice(possible_moves)
-            return random_move, nodes[random_move], 1
-        possible_moves.sort(key=lambda m: nodes[m].rating, reverse=current_strategy.is_maximizing)
-        num_visited_nodes = 0
-        optimal_move = None  # type: Optional[Move]
-        for current_move in possible_moves:
-            current_node = nodes[current_move]
-            if not current_node.is_leaf and mini_max.should_recurse():
-                mini_max.down()
-                _, child_node, child_num_visited = AlphaBetaPruning._apply_mini_max(
-                    mini_max, current_node.new_game_state, alpha, beta)
-                mini_max.up()
-                num_visited_nodes += child_num_visited
-                current_node.depth = child_node.depth
-                current_node.rating = child_node.rating
-            else:
-                num_visited_nodes += 1
-            if current_strategy.is_maximizing:
-                if optimal_move is None or current_node.rating > alpha:
-                    optimal_move = current_move
-                    alpha = current_node.rating
-                    if alpha >= beta or alpha == current_strategy.win_rating:
-                        break  # cut-off!
-            else:
-                if optimal_move is None or current_node.rating < beta:
-                    optimal_move = current_move
-                    beta = current_node.rating
-                    if beta <= alpha or beta == current_strategy.win_rating:
-                        break  # cut-off!
-        assert optimal_move is not None
-        return optimal_move, nodes[optimal_move], num_visited_nodes
-
-    @staticmethod
-    def _eval_move(mini_max: _MiniMax, game_state: ShiftagoExpress, move: Move) -> _Node:
-        is_leaf = False
-        rating = 0.0
-        cloned_game_state = game_state.clone()
-        game_end_condition = cloned_game_state.apply_move(move)
-        if game_end_condition is not None:
-            is_leaf = True
-            if game_end_condition.winner is not None:
-                rating = mini_max.current_strategy.win_rating
-        else:
-            player_results = cloned_game_state.analyze()
-            assert game_state.current_player, "No current player!"
-            current_player_result = player_results[game_state.current_player]
-            opponent_result = player_results[AlphaBetaPruning._current_opponent(game_state)]
-            winning_line_length = game_state.winning_line_length
-            win_rating = mini_max.current_strategy.win_rating
-            for i in range(winning_line_length, 1, -1):
-                rating += (len(current_player_result[i]) - len(opponent_result[i])
-                           ) * _pow10(-(winning_line_length - i + 1)) * win_rating
-        return _Node(cloned_game_state, is_leaf, mini_max.current_depth, rating)
-
-    @staticmethod
-    def _current_opponent(game_state: ShiftagoExpress) -> Colour:
-        players = game_state.players
-        return players[1 if players.index(game_state.current_player) == 0 else 0]
