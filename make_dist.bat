f564d1f953f5029d874618b9dd4583a384b9c9ec
Thomas Schaper <genius.magnus@tutanota.com>
2024-01-19 18:41:09 +0100

    Installer generates one file now.

--- make_dist.bat
+++ make_dist.bat
@@ -6,14 +6,14 @@ set scripts_dir=.venv\Scripts
 rem generate spec file
 set app_name=shiftago-qt
 set images_dir=shiftago/ui/images
-%scripts_dir%\pyi-makespec entry_point.py --name=%app_name% --noconsole^
+%scripts_dir%\pyi-makespec entry_point.py --name=%app_name% --noconsole --onefile^
     --add-data "%images_dir%/*.png:%images_dir%"^
     --add-data "%images_dir%/*.jpg:%images_dir%"
 
 rem enhance spec file
 set spec_file=%app_name%.spec
 echo import shutil>>%spec_file%
-echo shutil.copy('shiftago-qt.cfg', f'{DISTPATH}/{specnm}')>>%spec_file%
+echo shutil.copy('shiftago-qt.cfg', f'{DISTPATH}')>>%spec_file%
 
 rem run installer with enhanced spec file
 %scripts_dir%\pyinstaller %spec_file% --clean --noconfirm
