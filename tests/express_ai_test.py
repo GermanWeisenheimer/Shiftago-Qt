5e0c0ace65abd2b26a3f57ce1acc0b4636a5d7e5
Thomas Schaper <genius.magnus@tutanota.com>
2024-01-22 23:23:40 +0100

    AlphaBetaPruning test case added.

--- tests/express_ai_test.py
+++ tests/express_ai_test.py
@@ -3,7 +3,7 @@ import unittest
 import logging
 from shiftago.core import Colour, Side, Move
 from shiftago.core.express import ShiftagoExpress
-from shiftago.core.express_ai import AlphaBetaPruning
+from shiftago.core.express_ai import SkillLevel, AlphaBetaPruning
 from tests import TestDataLoader
 
 
@@ -22,9 +22,17 @@ class AlphaBetaPruningTest(unittest.TestCase):
         express_game.apply_move(move)
         print(express_game)
 
-    def test_alpha_beta_pruning(self):
+    def test_1_alpha_beta_pruning(self):
         with TestDataLoader(ShiftagoExpress, 'express_ai_test1.json') as express_game:
             print("\n{0}".format(express_game))
             move = AlphaBetaPruning().select_move(express_game)
-            self.assertIn(move, [Move(Side.BOTTOM, 1), Move(Side.BOTTOM, 2), Move(Side.BOTTOM, 4)])
+            self.assertIn(move, (Move(Side.BOTTOM, 1), Move(Side.BOTTOM, 2), Move(Side.BOTTOM, 4)))
             print("Move: {0}".format(move))
+
+    def test_2_alpha_beta_pruning(self):
+        with TestDataLoader(ShiftagoExpress, 'express_ai_test2.json') as express_game:
+            print("\n{0}".format(express_game))
+            for skill_level in (SkillLevel.EXPERT, SkillLevel.GRANDMASTER):
+                move = AlphaBetaPruning(skill_level).select_move(express_game)
+                self.assertIn(move, (Move(Side.TOP, 1), Move(Side.TOP, 4)))
+                print("Move: {0}".format(move))
